a42bcf3e625e9383e37d1edbe75cd67f96e8dc90
YuCheng-Guan <75397694+YuCheng-Guan@users.noreply.github.com>
2020-12-10 07:51:52 +0800

    Update identification.php

--- identification.php
+++ identification.php
@@ -5,13 +5,12 @@
 $password = $_GET["password"];
 $index_url = 'https://web-guan.herokuapp.com/';
 $passwordarray = array('郎窯紅穿帶瓶','侍衛','JAY','The ship repairer is actually a spy from B chamber of commerce.');
-$img_src = array(0=>array(1=>'/i.imgur.com/AHvrGdu.png',2=>'/i.imgur.com/fh3gJBY.png'),
-                 1=>array(1=>'/i.imgur.com/F0QIFuw.png',2=>'/i.imgur.com/G1NyXIO.png'),
-                 2=>array(1=>'/i.imgur.com/6g5OGd3.png',2=>'/i.imgur.com/Qr0MyZR.png'),
-                 3=>array(1=>'/i.imgur.com/srPtUge.png',2=>'/i.imgur.com/dIrGQLS.png'),
-                 4=>array(1=>'/i.imgur.com/qCyNVhZ.jpg',2=>'/i.imgur.com/e4N2dMF.jpg')
+$img_src = array(0=>array(1=>'/i.imgur.com/F0QIFuw.png',2=>'/i.imgur.com/G1NyXIO.png'),
+                 1=>array(1=>'/i.imgur.com/6g5OGd3.png',2=>'/i.imgur.com/Qr0MyZR.png'),
+                 2=>array(1=>'/i.imgur.com/srPtUge.png',2=>'/i.imgur.com/dIrGQLS.png'),
+                 3=>array(1=>'/i.imgur.com/qCyNVhZ.jpg',2=>'/i.imgur.com/e4N2dMF.jpg')
                 );
-for($i=0;$i<6;$i++){
+for($i=0;$i<5;$i++){
     if($i<=4 & $password!=''){
         if ($password == $passwordarray[$i]){
             echo "<img src= "."\"https:"."/" . $img_src[$i][1]."\""."style="."\""."display: block;margin-left: auto;margin-right: auto;width: 50%;","\">","<br>";
